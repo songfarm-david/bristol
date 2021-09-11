d678ca7b69d9fdfa28df28d52a814f4f30a56702
Dave Gaskin <davidburkegaskin@gmail.com>
2021-09-10 18:38:52 -0700

    add internal link to hair colouring page; update body link style

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import BrandBanner from "../components/theme/brand_banner"
 import Carousel from "../components/UI/gallery"
 
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 export default function Home({location}) {
 
@@ -15,7 +16,7 @@ export default function Home({location}) {
             <h2>We Style Your Hair With Years of Experience</h2>
             <section className={"grid_section"}>
                 <div>
-                    <p className={"text_italic"}>We are Bristol Town located in Victoria BC. Whether you’re looking for a trim or a transformation, Bristol Town Hair Fashions in Cadboro Bay has the stylist for you. We’ve assembled a team of specialists with a range of talents: from the latest cutting-edge styles, to traditional looks that suit your features and personality, we do it all. Our team of hair-care professionals also includes talented colourists who can help you add a jolt to your look, or restore the natural colour of your younger years.</p>
+                    <p className={"text_italic"}>We are Bristol Town located in Victoria BC. Whether you’re looking for a trim or a transformation, Bristol Town Hair Fashions in Cadboro Bay has the stylist for you. We’ve assembled a team of specialists with a range of talents: from the latest cutting-edge styles, to traditional looks that suit your features and personality, we do it all. Our team of hair-care professionals also includes <Link to="/hair-colouring">talented colourists</Link> who can help you add a jolt to your look, or restore the natural colour of your younger years.</p>
                 </div>
                 <div className={"button_group"}>
                     <ThemeButton />
